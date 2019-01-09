e223582d658926db47f650bc4b6ee242853f7091
Sagarrokade006 <sagarrokade006@gmail.com>
2019-01-09 14:03:36 +0000

    build: Automatically replace old-style kernel header includes with new style kernel headers
    Since we do this via soong and the result is a header library, just replace all
    legacy include paths that point to the old header location with calls to the new
    header library. Since we no longer have the legacy include, we can also remove
    the additional dependency.
    
    This reverts commit fa798218e5b7f96e12ab3acc9d47a3f26a140777.
    
    Change-Id: I716955534e50831e6568ca01e480aa8b90075d92

--- core/config.mk
+++ core/config.mk
@@ -230,9 +230,8 @@ $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 $(call project-set-path-variant,recovery,RECOVERY_VARIANT,bootable/recovery)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(CUSTOM_BUILD),)
-include vendor/pearl/config/BoardConfigLean.mk
-endif
+
+include vendor/pearl/config/BoardConfigPearl.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
@@ -1117,7 +1116,6 @@ endif
 
 # Rules for MTK targets
 -include $(TOPDIR)vendor/pearl/build/core/mtk_target.mk
-endif
 
 
 include $(BUILD_SYSTEM)/dumpvar.mk
