aosp-mirror/platform_build

releasetools: Make merge_target_files and its test Python 3 compatible.
And a few minor clean-ups to the styling.

Bug: 131631303
Test: python -m unittest test_merge_target_files
Test: python3 -m unittest test_merge_target_files
Test: Use `python merge_target_files` to merge two target_files zips.
Test: Use `python3 merge_target_files` to merge two target_files zips.
Change-Id: I8502dfb243408f658d022e8d5e5fbb60066e4ff0

tools/releasetools/test_merge_target_files.py
@@ -98,7 +98,8 @@ class MergeTargetFilesTest(test_utils.ReleaseToolsTestCase):
         'ROOT/*',
         'SYSTEM/*',
     ]
-    self.assertItemsEqual(framework_item_list, expected_framework_item_list)
+    self.assertEqual(sorted(framework_item_list),
+                     sorted(expected_framework_item_list))
 
   def test_validate_config_lists_ReturnsFalseIfMissingDefaultItem(self):
     framework_item_list = list(DEFAULT_FRAMEWORK_ITEM_LIST)

PearlOS/platform_build_make

tools/releasetools/test_merge_target_files.py
@@ -43,7 +43,8 @@ class MergeTargetFilesTest(test_utils.ReleaseToolsTestCase):
         'ROOT/*',
         'SYSTEM/*',
     ]
-    self.assertItemsEqual(system_item_list, expected_system_item_list)
+    self.assertEqual(sorted(system_item_list),
+                     sorted(expected_system_item_list))
 
   def test_validate_config_lists_ReturnsFalseIfMissingDefaultItem(self):
     system_item_list = default_system_item_list[:]